165628f3417ebdf4653d0cfd2152dd0c9e963849
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2025-03-03 17:56:24 +0000

    Update models_matrix.py
    update to bayes matrix

--- models_matrix.py
+++ models_matrix.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 import numpy as np
-from models import y_test, rf_prediction, xgb_prediction, gb_prediction
+from models import y_test, rf_prediction, nb_prediction, gb_prediction
 
 #  function to plot matrix
 def plot_matrix(y_true, y_pred, model_name):
@@ -18,7 +18,7 @@ def plot_matrix(y_true, y_pred, model_name):
 plot_matrix(y_test, rf_prediction, 'Random Forest')
 
 # XGBoost confusion matrix
-plot_matrix(y_test, xgb_prediction, 'XGBoost')
+plot_matrix(y_test, nb_prediction, 'XGBoost')
 
 # Gradient Boosting confusion matrix
 plot_matrix(y_test, gb_prediction, 'Gradient Boosting')
