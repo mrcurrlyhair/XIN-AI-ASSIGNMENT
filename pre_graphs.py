75ec33338be4fbf4c978c88f77fcd4a8e1513047
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2025-03-10 22:01:53 +0000

    Update pre_graphs.py
    misc spellings and format

--- pre_graphs.py
+++ pre_graphs.py
@@ -37,9 +37,9 @@ while True:
                 percentage.text(i, fatal_count + 1, f'{fatal_percentage:.1f}%', ha='center', fontsize=10, color='black')
 
 
-        plt.xlabel('Number of vehicles  Involved')
+        plt.xlabel('Number of Vehicles Involved')
         plt.ylabel('Count')
-        plt.title('Total Accidents vs Fatal Accidents by Number of vehicles Involved')
+        plt.title('Total Accidents vs Fatal Accidents by Number of Vehicles Involved')
         plt.legend()
         plt.show()
         plt.close()
